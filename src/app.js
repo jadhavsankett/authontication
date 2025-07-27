51e5f155168e32030a6ae4793ba2750ba3b83a2d
student sanket25 <jadhavsanket5656@gamil.com>
2025-07-27 14:05:54 -0400

    remove

--- src/app.js
+++ src/app.js
@@ -10,17 +10,3 @@ app.use('/auth',authroute)
 
 module.exports = app
 
-/*
-const express = require('express');
-const authroute = require('./routes/auth.routes')
-
-
-const app = express()
-
-
-app.use('/auth',authroute)
-
-
-module.exports = app
-
-*/
