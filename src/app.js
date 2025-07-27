44c1c74da0d0320c9d4b60625d6e69e39d714487
student sanket25 <jadhavsanket5656@gamil.com>
2025-07-27 14:05:15 -0400

    commited

--- src/app.js
+++ src/app.js
@@ -10,3 +10,17 @@ app.use('/auth',authroute)
 
 module.exports = app
 
+/*
+const express = require('express');
+const authroute = require('./routes/auth.routes')
+
+
+const app = express()
+
+
+app.use('/auth',authroute)
+
+
+module.exports = app
+
+*/
