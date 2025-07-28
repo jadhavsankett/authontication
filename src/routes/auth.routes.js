52f8be4af28bf294bc6752e4c472d23a96b2e985
student sanket25 <jadhavsanket5656@gamil.com>
2025-07-28 07:01:30 -0400

    authantication jws token day 13

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const usermodel = require('../models/user.model')
+const jwt = require('jsonwebtoken')
 
 const router = express.Router()
 
@@ -12,9 +13,18 @@ router.post('/register',async(req,res)=>{    //this code is register user
         password
     })
 
+//this step the create the token process
+
+    const token = jwt.sign({
+      id:user._id,    // every user quien identify fro use id 
+      
+    },process.env.JWT_SECRET)   //one set the secret not the change his alway this secret 
+
+
     res.status(201).json({
         Message:'register user succfully',
-        user
+        user,
+        token
     })
 })
 
@@ -43,6 +53,41 @@ router.post('/login',async(req,res)=>{      //comple the login user and corrcet 
     res.status(201).json({
         message:'login user successfully'
     })
+})
+
+
+
+// this token send the req.body 
+router.get('/user',async(req,res)=>{
+   const {token} = req.body
+
+   if(!token){
+     return res.status(401).json({
+        message:"unauthorized"
+     })
+   }
+
+   try{
+       
+       const decoded = jwt.verify(token , process.env.JWT_SECRET)
+
+       const user = await usermodel.findOne({
+        _id:decoded.id
+       }).select("-password -__v")     //this step is the removeing perticular data 
+
+       res.status(201).json({
+        massage:'user data fetched successfully',
+        user
+       })
+
+       res.send(decoded)
+
+   }catch{
+    return res.status(401).json({
+        message:'unauthorized token cereated'
+    })
+   }
+
 })
 
 
